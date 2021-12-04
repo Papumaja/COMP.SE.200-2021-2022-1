488d02f0ef3356e676c70a4714c7c7bd4ed51ccf
Henri Seppä <henri.seppae@gmail.com>
2021-12-05 00:28:24 +0200

    Added more tests for filter function

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -10,7 +10,16 @@ test('Array has many different datatypes', () =>{
     expect(filter(['hello',2,4.21321,5,7],(value)=>Number.isInteger(value))).toStrictEqual([2,5,7]);
 });
 
+test('Array has one item', () =>{
+    expect(filter(['word'],(value)=>value==='word')).toStrictEqual(['word']);
+});
+
+
 //predicate testing
 test('Predicate returns null', () =>{
     expect(filter(['hello',2,4.21321,5,7],()=>null)).toStrictEqual([[]]);
+});
+
+test('Predicate throws error', () =>{
+    expect(filter(['hello',2,4.21321,5,7],(value)=>{throw 'error'})).toStrictEqual([[]]);
 });
