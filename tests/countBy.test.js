f8518fc363fb17a49190baad9d4071044579be1e
Henri Seppä <henri.seppae@gmail.com>
2021-12-04 13:10:31 +0200

    Implemented first tests for countBy

--- tests/countBy.test.js
+++ tests/countBy.test.js
@@ -1,1 +1,28 @@
 import countBy from '../src/countBy'
+
+//array
+
+test('Array has one item', () =>{
+    expect(countBy(['test'],(value)=>value==='test')).toStrictEqual({'true':1});
+});
+
+test('Array has multiple items', () =>{
+    expect(countBy(['test','word','hello','test'],value=>value==='test')).toStrictEqual({'true':2,'false':2});
+});
+
+test('Array is empty', () =>{
+    expect(countBy([],(value)=>value==='test')).toStrictEqual({});
+});
+
+//object
+
+const persons = [{'name':'adam', 'alive':true}, {'name':'betty', 'alive':true}, {'name':'charlie', 'alive':false}]
+
+test('Collection has multiple values', () =>{
+    expect(countBy(persons,value=>value.alive)).toStrictEqual({'true':2,'false':1});
+});
+
+
+test('Collection has one item', () =>{
+    expect(countBy([{'name':'adam', 'alive':true}],value=>value.alive)).toStrictEqual({'true':1});
+});
